c2ee5d0bc42098a0eec177e5a49d98fd5438637f
INDOnimous <40731452+INDOnimous@users.noreply.github.com>
2019-05-13 13:16:12 +0700

    Add files via upload

---
+++ INDOwget-learn.sh
@@ -0,0 +1,16 @@
+#!/ust/etc/Bash
+#
+#
+#
+if
+then
+apt-get update 
+apt-get upgrade 
+apt-get install PHP
+apt-get install curl
+apt-get install nano
+figlet 'INDOtutoral |lolcat'
+git clone https://github.com/INDOnimous/INDOfish
+echo " Thanks to watching this video ✓✓"
+echo " Suport me with SUBSCRIBE and Share ✓✓"
+fi
